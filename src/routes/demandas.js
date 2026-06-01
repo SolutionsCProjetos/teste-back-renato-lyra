0a8b51c18141ac7a8a352eab0aed8d11a189a4d7
SolutionsCProjetos <SolutionsCProjetos@hotmail.com>
2026-05-31 23:13:02 -0300

    Update demandas.js

--- src/routes/demandas.js
+++ src/routes/demandas.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { PrismaClient } = require('@prisma/client');
 const router = express.Router();
-const prisma = new PrismaClient();
+const prisma = require('../prismaClient')
 
 const authMiddleware = require('../../middlewares/auth');
 router.use(authMiddleware);
